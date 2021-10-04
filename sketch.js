e06bbfe4bce9ac3da3519b53ef35d7265b6c71a7
vedant113 <85504760+vedant113@users.noreply.github.com>
2021-10-04 20:47:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ function setup(){
 createCanvas(800,800);
 
 target1 = new Target(400,400,40,40);
+target1.setVelocityY = 2,-2;
+target1.setVelocityX = -2,2;
+target1.lifeTime = 5;
 }
 
 function Draw(){
